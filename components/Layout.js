a7ae9efbcba26d68dfb549d2540dfd563df08801
Taha Selim <tahaselimaksakal@gmail.com>
2019-09-06 15:09:08 +0300

    css to scss

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import Navbar from './Navbar';
 import '../styles/index.scss'
+import '../styles/nprogress.scss'
 
 const Layout = props => (
 	<div>
 		<Head>
 			<title>Tv Show App</title>
-			<link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css" />
 		</Head>
 		<Navbar/>
